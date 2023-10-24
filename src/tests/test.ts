e88637219e4e07ce77f1e21624e470dad3cf432d
Oleksii Khomiakov <alex.xag1987@gmail.com>
2023-10-24 03:35:33 +0300

    Update test.ts

--- src/tests/test.ts
+++ src/tests/test.ts
@@ -1,5 +1,44 @@
-function add(num1: number, num2: number) {
-  return num1 + num2;
-}
+// function add(num1: number, num2: number) {
+//   return num1 + num2;
+// }
 
-add(1, 1);
+// add(1, 1);
+
+// let total: number = 100;
+// let firstName: string = "Alex";
+// let isActive: boolean = false;
+// let empty: null = null;
+// let undf: undefined = undefined;
+
+// let age = 10;
+
+// age = "12";
+
+// const numbers: number[] = [1, 2, 3, 4, 5, 6, 3];
+// numbers.push("12");
+
+const user: { name: string; age: number } = {
+  name: "Alex",
+  age: 36,
+};
+
+// user.age = '37';
+// user.name = 1;
+
+type User = {
+  name: string;
+  age: number; // | string;
+};
+
+const user1: User = {
+  name: "Alex",
+  age: 36,
+};
+
+// user1.age = '37';
+// user1.name = 1;
+
+// type EventType = 'lesson' | 'deadline';
+// const newEvent: EventType = 'deadline'
+
+type Size = "small" | "medium" | "large";
